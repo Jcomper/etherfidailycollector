8cb76cd9ee5f801f68167090e338af3928146c7a
Jcomper <jcomp.jcomper@gmail.com>
2024-03-02 21:41:52 +0200

    check for points property exists

--- main.py
+++ main.py
@@ -37,24 +37,40 @@ async def get_info(session, wallet, retry = 0):
         async with session.get(get_etherfi_url, headers=etherfi_headers, ssl=False, proxy=proxy, timeout=10) as resp:
             resp_json = await resp.json(content_type=None)
             loyaltyPoints = resp_json['loyaltyPoints']
-            loyaltyPoints += resp_json['balancer']["loyaltyPoints"]
-            loyaltyPoints += resp_json['curve']["loyaltyPoints"]
-            loyaltyPoints += resp_json['gravita']["loyaltyPoints"]
-            loyaltyPoints += resp_json['maverick']["loyaltyPoints"]
-            loyaltyPoints += resp_json['sommelier']["loyaltyPoints"]
-            loyaltyPoints += resp_json['pendle']["loyaltyPoints"]
-            loyaltyPoints += resp_json['term']["loyaltyPoints"]
-            loyaltyPoints += resp_json['mega']["loyaltyPoints"]
+            if ('balancer' in resp_json):
+                loyaltyPoints += resp_json['balancer']["loyaltyPoints"]
+            if ('curve' in resp_json):
+                loyaltyPoints += resp_json['curve']["loyaltyPoints"]
+            if ('gravita' in resp_json):
+                loyaltyPoints += resp_json['gravita']["loyaltyPoints"]
+            if ('maverick' in resp_json):
+                loyaltyPoints += resp_json['maverick']["loyaltyPoints"]
+            if ('sommelier' in resp_json):
+                loyaltyPoints += resp_json['sommelier']["loyaltyPoints"]
+            if ('pendle' in resp_json):
+                loyaltyPoints += resp_json['pendle']["loyaltyPoints"]
+            if ('term' in resp_json):
+                loyaltyPoints += resp_json['term']["loyaltyPoints"]
+            if ('mega' in resp_json):
+                loyaltyPoints += resp_json['mega']["loyaltyPoints"]
             
             eigenlayerPoints = resp_json['eigenlayerPoints']
-            eigenlayerPoints += resp_json['balancer']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['curve']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['gravita']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['maverick']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['sommelier']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['pendle']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['term']["eigenlayerPoints"]
-            eigenlayerPoints += resp_json['mega']["eigenlayerPoints"]
+            if ('balancer' in resp_json):
+                eigenlayerPoints += resp_json['balancer']["eigenlayerPoints"]
+            if ('curve' in resp_json):
+                eigenlayerPoints += resp_json['curve']["eigenlayerPoints"]
+            if ('gravita' in resp_json):
+                eigenlayerPoints += resp_json['gravita']["eigenlayerPoints"]
+            if ('maverick' in resp_json):
+                eigenlayerPoints += resp_json['maverick']["eigenlayerPoints"]
+            if ('sommelier' in resp_json):
+                eigenlayerPoints += resp_json['sommelier']["eigenlayerPoints"]
+            if ('pendle' in resp_json):
+                eigenlayerPoints += resp_json['pendle']["eigenlayerPoints"]
+            if ('term' in resp_json):
+                eigenlayerPoints += resp_json['term']["eigenlayerPoints"]
+            if ('mega' in resp_json):
+                eigenlayerPoints += resp_json['mega']["eigenlayerPoints"]
             
             row_data[wallet]['etherfi']['loyaltyPoints'] = loyaltyPoints
             row_data[wallet]['etherfi']['eigenlayerPoints'] = eigenlayerPoints
